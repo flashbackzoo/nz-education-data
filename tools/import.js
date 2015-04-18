84719b71afa38fc4755b9e738b1a6952ffd308b4
David Craig <david@davidcraig.co.nz>
2015-04-19 10:17:16 +1200

    Update JSON formatting in importer

--- tools/import.js
+++ tools/import.js
@@ -10,13 +10,41 @@ var writer = fs.createWriteStream(outputXlsxFile);
 writer.on('finish', function () {
     console.log('converting data to JSON...');
     var workbook = XLSX.readFile(outputXlsxFile),
+        schools,
+        school,
         output = {};
 
     for (var sheetName in workbook.Sheets) {
-        output[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        schools = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+        // Group subjects.
+        for (var i = 0; i < schools.length; i += 1) {
+            school = {
+                id: schools[i]['school id'],
+                name: schools[i]['school name'],
+                subjects: {}
+            };
+
+            for (var columnName in schools[i]) {
+                // Check if we're dealing with a subject.
+                if (columnName !== 'school id' && columnName !== 'school name') {
+                    if (schools[i][columnName] === ' ') {
+                        school.subjects[columnName] = null;
+                    } else {
+                        school.subjects[columnName] = parseInt(schools[i][columnName], 10);
+                    }
+                }
+            }
+
+            schools[i] = school;
+        }
+
+        // Group schools
+        output[sheetName] = {
+            schools: schools
+        };
     }
 
-    
     fs.writeFile('data.json', JSON.stringify(output), function (err) {
         if (err) {
             console.log(err);
