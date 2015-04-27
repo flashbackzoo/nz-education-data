b6b1e7098ef199a1a7a3a8d93004959161efcaec
David Craig <david@davidcraig.co.nz>
2015-04-27 15:03:08 +1200

    Added year API url

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Hapi = require('hapi'),
+    fs = require('fs'),
+    constants = require('./constants'),
     secrets = require('./secrets');
 
 var server = new Hapi.Server();
@@ -34,24 +36,30 @@ server.route({
     handler: function (request, reply) {
         reply.file('data.json').header('Content-Type', 'application/json');
     },
-    config: {
-        cors: {
-            methods: ['GET']
-        },
-        cache: {
-            expiresIn: 60 * 60 * 24 * 7, // 1 week
-            privacy: 'private'
-        }
-    }
+    config: constants.API_ROUTE_CONFIG
 });
 
-server.views({
-    engines: {
-        jade: require('jade')
+server.route({
+    method: 'GET',
+    path: '/api/year/{id}',
+    handler: function (request, reply) {
+        fs.readFile('./data.json', 'utf8', function (err, data) {
+            var response;
+
+            if (err) {
+                console.log(err);
+                return reply.view('error');
+            }
+
+            response = JSON.parse(data)[request.params.id] || {};
+
+            reply(response).header('Content-Type', 'application/json');
+        });
     },
-    relativeTo: __dirname,
-    path: './views'
+    config: constants.API_ROUTE_CONFIG
 });
+
+server.views(constants.VIEW_OPTIONS);
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
